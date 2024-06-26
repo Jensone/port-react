09fd68329f986c2912e07d8fc684588094d828ad
Jensone <hello@jensone.com>
2024-06-26 16:27:04 +0200

    🔧 corriger(Skills.tsx) : changer le nom de la compétence "React JS" en "React" 🔧 corriger(Skills.tsx) : ajouter la gestion du choix de type de projet avec useState 🔧 corriger(Skills.tsx) : ajouter la fonctionnalité de filtrage des projets par choix de compétence

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+import Project from "../components/Project";
+
+/**
+ * Skills:
+ * Composant pour afficher les compétences,
+ * Il s'agit du contenu d'une page complète
+ * située sur la route : "/skills".
+ */
 export default function Skills() {
+    // On déclare une constant pour le choix du type de projet
+    const [choice, setChoice] = useState("All");
+    
+    
     const listSkills = [
         {
             name: "Symfony",
@@ -11,7 +24,7 @@ export default function Skills() {
             desc: "Node FullStack Framework for modern and robust web applications",
         },
         {
-            name: "React JS",
+            name: "React",
             image: "https://api.iconify.design/logos:react.svg?color=%23ffffff",
             desc: "JS Library for building UI web applications",
         },
@@ -22,25 +35,35 @@ export default function Skills() {
         },
     ];
     return (
-        <section className="flex gap-5 px-5 py-10">
-            <div className="w-1/3">
-                <h1 className="mb-5 text-3xl font-bold text-white">Skillset</h1>
-                <p className="leading-relaxed text-slate-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non libero, sapiente ipsum accusantium eligendi consectetur repellat nobis nisi optio rem itaque, hic atque doloribus saepe laboriosam corrupti omnis tempore possimus recusandae? Molestias error tempore quo! Adipisci temporibus, voluptates neque officiis quam voluptatum consectetur ad numquam doloribus accusamus, alias dolore ducimus.</p>
-            </div>
-            <ul className="grid w-2/3 grid-cols-2">
-                {
-                    listSkills.map((skill) => (
-                        <li key={ skill.name}>
-                            <img 
-                                className="transition ease-in-out hover:scale-125"
-                                src={ skill.image } alt={ skill.name + " logo" } width={ 32 } 
-                            />
-                            <h3 className="text-lg font-semibold text-white">{ skill.name }</h3>
-                            <p className="text-slate-600">{ skill.desc }</p>
-                        </li>
-                    ))
-                }
-            </ul>
-        </section>
+        <>
+            <section className="flex gap-5 px-5 py-10">
+                <div className="w-1/3">
+                    <h1 className="mb-5 text-3xl font-bold text-white">Skillset</h1>
+                    <p className="leading-relaxed text-slate-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non libero, sapiente ipsum accusantium eligendi consectetur repellat nobis nisi optio rem itaque, hic atque doloribus saepe laboriosam corrupti omnis tempore possimus recusandae? Molestias error tempore quo! Adipisci temporibus, voluptates neque officiis quam voluptatum consectetur ad numquam doloribus accusamus, alias dolore ducimus.</p>
+                </div>
+                <ul className="grid w-2/3 grid-cols-2">
+                    {
+                        listSkills.map((skill) => (
+                            <li 
+                            className="cursor-pointer"
+                            key={ skill.name}
+                            data-choice={ skill.name }
+                            onClick={() => setChoice(skill.name)}
+                            >
+                                <img 
+                                    className="transition ease-in-out hover:scale-125"
+                                    src={ skill.image } alt={ skill.name + " logo" } width={ 32 } 
+                                />
+                                <h3 className="text-lg font-semibold text-white">{ skill.name }</h3>
+                                <p className="text-slate-600">{ skill.desc }</p>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </section>
+            <section>
+                <Project prop={choice}/>
+            </section>
+        </>
     )
 }
